47a3dbd82920e1d71576c65ea2c23e89a301508f
sombeer <sombeery@gmail.com>
2022-10-16 15:10:13 +0530

    static files url

--- HE_website/he_admin/urls.py
+++ HE_website/he_admin/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from he_admin import views
 
@@ -23,3 +25,5 @@ urlpatterns = [
     path('status-product/<int:id>/',views.StatusProduct,name='statusproduct'),
     
 ]
++ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
++ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
